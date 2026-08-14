0be46d900ffe367c2f7618cbebd9eb9f76232a8a
vazgenarutyunyan7 <vazgenarutyunyan7@gmail.com>
2026-08-14 20:46:47 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'compass-v18';
+const CACHE_NAME = 'compass-v19';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 // 1. Установка и мгновенное добавление новых файлов

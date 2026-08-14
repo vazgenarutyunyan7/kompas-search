2958b565709703709e611d2394d15c60f8377119
vazgenarutyunyan7 <vazgenarutyunyan7@gmail.com>
2026-08-14 19:06:29 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,31 @@
-const CACHE_NAME = 'compass-v2'; // Поменяйте v1 на v2
+const CACHE_NAME = 'compass-v3';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
+// 1. Установка и мгновенное добавление новых файлов
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Принудительно активируем новый скрипт
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
+// 2. Очистка СТАРОГО кэша (v1, v2 и т.д.) при запуске
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Удаляем старый кэш:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Захватываем контроль над страницей
+  );
+});
+
+// 3. Отдача файлов из нового кэша
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
